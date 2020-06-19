97a494d966b5aa7045697ae6a18f84cd6a42f631
rinorukshini <rinor.ukshini@icloud.com>
2020-06-20 00:57:42 +0200

    Ndryshimet ne fragmente si dhe krijimi i disa prej tyre se bashku me firebase

--- app/src/main/java/com/example/projekti/MainActivity.java
+++ app/src/main/java/com/example/projekti/MainActivity.java
@@ -1,13 +1,19 @@
 package com.example.projekti;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Intent;
 import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.Switch;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     ConstraintLayout linearLayout;
@@ -35,4 +41,30 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.menuu,menu);
+        return true;
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.item1:
+                Toast.makeText(this,"Item 1 u selektu",Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item2:
+                Intent intent=new Intent(this,LoginActivity.class);
+                startActivity(intent);
+                return true;
+            case R.id.item3:
+                Intent intent1=new Intent(this,Regjistrimi.class);
+                startActivity(intent1);
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
--- app/src/main/java/com/example/projekti/ProfilActivity.java
+++ app/src/main/java/com/example/projekti/ProfilActivity.java
@@ -1,14 +1,95 @@
 package com.example.projekti;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class ProfilActivity extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
+
+public class ProfilActivity extends AppCompatActivity implements Aktivitet_frag.ItemSelected {
+
+    TextView tvDescription;
+    String[] descriptions;
+    ImageView imageView;
+    private FirebaseDatabase fireBaseDatabase=FirebaseDatabase.getInstance();
+    private DatabaseReference databaseReference=fireBaseDatabase.getReference();
+    DatabaseReference first;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profil);
+
+        tvDescription = findViewById(R.id.tvDescription);
+
+        descriptions = getResources().getStringArray(R.array.descriptions);
+
+        imageView=findViewById(R.id.foto);
+
+        //the phone is in portrait mode
+        if (findViewById(R.id.layout_portrait) != null) {
+            FragmentManager manager = this.getSupportFragmentManager();
+
+            manager.beginTransaction()
+                    .hide(manager.findFragmentById(R.id.detailFrag))
+                    .show(manager.findFragmentById(R.id.listFrag))
+                    .commit();
+        }
+
+        //the phone is in landscape mode
+        if (findViewById(R.id.layout_land) != null) {
+            FragmentManager manager = this.getSupportFragmentManager();
+
+            manager.beginTransaction()
+                    .show(manager.findFragmentById(R.id.detailFrag))
+                    .show(manager.findFragmentById(R.id.listFrag))
+                    .commit();
+        }
+
     }
-}
+
+    @Override
+    public void onItemSelected(int index) {
+        tvDescription.setText(descriptions[index]);
+
+        if (index==0){
+          first=databaseReference.child("image");
+        }
+        else if (index==1) {
+            first=databaseReference.child("foto");
+        }
+        else if (index==2){
+            first=databaseReference.child("foto1");
+        }
+        else if (index==3){
+            first=databaseReference.child("foto2");
+        }
+        else if (index==4){
+            first=databaseReference.child("foto3");
+        }
+        else if (index==5){
+            first=databaseReference.child("foto4");
+        }
+
+
+            first.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    String link=dataSnapshot.getValue(String.class);
+                    Picasso.get().load(link).into(imageView);
+                }
